8135dac63dab0abbd9baf9c665e49870acd981db
nlpxdc <nlpx_dc@hotmail.com>
2021-04-03 21:25:16 +0800

    first init

---
+++ src/main/java/io/cjf/jimservice/JimServiceApplication.java
@@ -0,0 +1,13 @@
+package io.cjf.jimservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JimServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JimServiceApplication.class, args);
+    }
+
+}
