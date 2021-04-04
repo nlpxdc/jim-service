cb92cf4d89ee89f9385128101713650b0fd704a8
nlpxdc <nlpx_dc@hotmail.com>
2021-04-04 14:17:24 +0800

    fix

---
+++ src/main/java/io/cjf/jimservice/controller/UserBaseController.java
@@ -0,0 +1,9 @@
+package io.cjf.jimservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/userBase")
+public class UserBaseController {
+}
