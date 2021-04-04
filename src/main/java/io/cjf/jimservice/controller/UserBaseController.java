eab2dc3fc587dcdf353d42045b68aa3b97c89f68
nlpxdc <nlpx_dc@hotmail.com>
2021-04-04 14:45:27 +0800

    add user base get profile

--- src/main/java/io/cjf/jimservice/controller/UserBaseController.java
+++ src/main/java/io/cjf/jimservice/controller/UserBaseController.java
@@ -1,9 +1,34 @@
 package io.cjf.jimservice.controller;
 
+import io.cjf.jimservice.exception.ClientException;
+import io.cjf.jimservice.po.User;
+import io.cjf.jimservice.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/userBase")
 public class UserBaseController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/getProfile")
+    public User getProfile(@RequestAttribute String currentUserId) throws ClientException {
+        User user = userService.load(currentUserId);
+        if (user == null) {
+            throw new ClientException("no user");
+        }
+        user.setLoginPassword(null);
+        return user;
+    }
+
+    @PostMapping("/updateProfile")
+    public void updateProfile() {
+
+    }
+
 }
