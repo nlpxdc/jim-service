baee5acb5f9962cca84ca3af5d861ec4537eadea
nlpxdc <nlpx_dc@hotmail.com>
2021-04-04 13:45:33 +0800

    add client filter

---
+++ src/main/java/io/cjf/jimservice/filter/ClientFilter.java
@@ -0,0 +1,29 @@
+package io.cjf.jimservice.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Order(1)
+@Component
+public class ClientFilter implements Filter {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        logger.info("url: {}", request.getRequestURI());
+        filterChain.doFilter(servletRequest, servletResponse);
+        return;
+    }
+
+}
