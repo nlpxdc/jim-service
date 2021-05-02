0862b6b1e613be3b6f7180b74329712fee55a7cb
nlpxdc <nlpx_dc@hotmail.com>
2021-05-02 19:33:52 +0800

    fix

--- src/main/java/io/cjf/jimservice/controller/UxyController.java
+++ src/main/java/io/cjf/jimservice/controller/UxyController.java
@@ -19,7 +19,6 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
 
 @RestController
 @RequestMapping("/uxy")
